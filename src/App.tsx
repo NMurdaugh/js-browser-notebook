9f30f7f5ccc10906e53fb85ed0086ee26fdcb36c
Nick <nmurdaugh42@gmail.com>
2023-08-19 18:10:27 -0500

    set up basic input box

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,27 @@
-function App() {
+import { useState } from 'react';
+
+const App = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = () => {
+    console.log(input);
+  };
+
   return (
     <>
-      <div>Hi</div>
+      <div>
+        <textarea
+          value={input}
+          onChange={(event) => setInput(event.target.value)}
+        ></textarea>
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+      </div>
     </>
   );
-}
+};
 
 export default App;
