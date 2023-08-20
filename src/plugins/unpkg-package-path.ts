27c6ec0990ca6ffeb4ace94d14a67944d379fa21
Nick <nmurdaugh42@gmail.com>
2023-08-19 23:56:02 -0500

    hardcoded url for proof of concept

--- src/plugins/unpkg-package-path.ts
+++ src/plugins/unpkg-package-path.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import * as esbuild from 'esbuild-wasm';
 
 export const unpkgPackagePath = () => {
@@ -6,7 +7,14 @@ export const unpkgPackagePath = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log('onResolve', args);
-        return { path: args.path, namespace: 'a' };
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        } else if (args.path === 'tiny-test-pkg') {
+          return {
+            path: 'https://unpkg.com/tiny-test-pkg@1.0.0/index.js',
+            namespace: 'a',
+          };
+        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,16 +24,17 @@ export const unpkgPackagePath = () => {
           return {
             loader: 'jsx',
             contents: `
-              import message from './message';
+              import message from 'tiny-test-pkg';
               console.log(message);
             `,
           };
-        } else {
-          return {
-            loader: 'jsx',
-            contents: 'export default "hi there!"',
-          };
         }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
       });
     },
   };
