4f026710d4ed21de6d4efe542eb30e5219e2c608
Nick <nmurdaugh42@gmail.com>
2023-08-20 15:44:15 -0500

    added typing to args

--- src/plugins/unpkg-package-path.ts
+++ src/plugins/unpkg-package-path.ts
@@ -10,7 +10,7 @@ export const unpkgPackagePath = () => {
   return {
     name: 'unpkg-package-path',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
+      build.onResolve({ filter: /.*/ }, async (args: esbuild.OnResolveArgs) => {
         console.log('onResolve', args);
         if (args.path === 'index.js') {
           return { path: args.path, namespace: 'a' };
@@ -29,7 +29,7 @@ export const unpkgPackagePath = () => {
         };
       });
 
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
+      build.onLoad({ filter: /.*/ }, async (args: esbuild.OnLoadArgs) => {
         console.log('onLoad', args);
 
         if (args.path === 'index.js') {
