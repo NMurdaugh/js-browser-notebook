19d7bc5c0efeb3527dc6dc7fd9e3eedc09daf6bc
Nick <nmurdaugh42@gmail.com>
2023-08-19 21:17:47 -0500

    implemented esbuild to bundle

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
+import startService from './plugins/esbuild';
 
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
-  const onClick = () => {
-    console.log(input);
+  const onClick = async () => {
+    const esBuildRef = await startService();
+    const result = await esBuildRef.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+    setCode(result.code);
   };
 
   return (
---
+++ src/plugins/esbuild.ts
@@ -0,0 +1,18 @@
+import * as esbuild from 'esbuild-wasm';
+
+const startService = (() => {
+  let esBuildRef: any;
+  return async () => {
+    if (esBuildRef) {
+      return esBuildRef;
+    } else {
+      await esbuild.initialize({
+        wasmURL: './esbuild.wasm',
+      });
+      esBuildRef = esbuild;
+      return esBuildRef;
+    }
+  };
+})();
+
+export default startService;
