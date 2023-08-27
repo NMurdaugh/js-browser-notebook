2de52c77ea38136d54cacfc1323885e684306ac0
Nick <nmurdaugh42@gmail.com>
2023-08-26 20:58:58 -0500

    added error handling

--- src/App.tsx
+++ src/App.tsx
@@ -32,16 +32,22 @@ const App = () => {
 
   const html = `
   <html>
-        <head></head>
-        <body>
-          <div id='root'></div>
-          <script>
-            window.addEventListener('message', (event) => {
-              console.log(event.data)
-            }, false)
-          </script>
-        </body>
-      </html>
+  <head></head>
+  <body>
+    <div id='root'></div>
+    <script>
+      window.addEventListener('message', (event) => {
+        try {
+          eval(event.data);
+        } catch (error) {
+          const root = document.querySelector('#root');
+          root.innerHTML = '<div style="color: red"><h4>Runtime Error:</h4>' + error + '</div>'
+          console.error(error)
+        }
+      }, false)
+    </script>
+  </body>
+</html>
   `;
 
   return (
