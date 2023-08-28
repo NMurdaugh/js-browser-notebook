d5afdae3eb10c50dcbd9a878f37a7edb67d91505
Nick <nmurdaugh42@gmail.com>
2023-08-27 20:16:00 -0500

    added prettier with options

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,6 @@
 import Editor, { OnChange, OnMount } from '@monaco-editor/react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
 import React, { useRef } from 'react';
 
 interface ICodeEditorProps {
@@ -14,27 +16,46 @@ const CodeEditor: React.FC<ICodeEditorProps> = ({ initialValue, onChange }) => {
     console.log(editorRef.current.getValue());
   };
 
+  const onFormatClick = () => {
+    const rawCode = editorRef.current.getValue();
+
+    const formattedCode = prettier.format(rawCode, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: true,
+      singleQuote: true,
+      bracketSpacing: true,
+      trailingComma: 'all',
+    });
+
+    editorRef.current.setValue(formattedCode);
+  };
+
   return (
-    <Editor
-      onMount={handleEditorDidMount}
-      onChange={onChange}
-      value={initialValue}
-      height='250px'
-      language='javascript'
-      theme='vs-dark'
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-        cursorBlinking: 'phase',
-        tabSize: 2,
-      }}
-    />
+    <>
+      <button onClick={onFormatClick}>Format</button>
+      <Editor
+        onMount={handleEditorDidMount}
+        onChange={onChange}
+        value={initialValue}
+        height='250px'
+        language='javascript'
+        theme='vs-dark'
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          cursorBlinking: 'phase',
+          tabSize: 2,
+        }}
+      />
+    </>
   );
 };
 
