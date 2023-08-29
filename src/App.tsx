f2fce81ef79864c9ed198657ae75d5afbb898e87
Nick <nmurdaugh42@gmail.com>
2023-08-28 22:01:40 -0500

    built component to load multiple editor/preview sets

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,9 @@
-import { useState } from 'react';
-import bundler from './bundler';
-import CodeEditor from './components/code-editor';
-import Preview from './components/preview';
+import CodeCell from './components/code-cell';
 
 const App = () => {
-  const [input, setInput] = useState('');
-  const [code, setCode] = useState('');
-
-  const onClick = async () => {
-    const bundledCode = await bundler(input);
-    setCode(bundledCode);
-  };
-
   return (
     <>
-      <div>
-        <CodeEditor
-          initialValue='const hey = "hello";'
-          onChange={(value) => {
-            if (value) setInput(value);
-          }}
-        />
-        <div>
-          <button onClick={onClick}>Submit</button>
-        </div>
-        <Preview code={code} />
-      </div>
+      <CodeCell />
     </>
   );
 };
---
+++ src/components/code-cell.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+import bundler from '../bundler';
+import CodeEditor from '../components/code-editor';
+import Preview from '../components/preview';
+
+const CodeCell = () => {
+  const [input, setInput] = useState('');
+  const [code, setCode] = useState('');
+
+  const onClick = async () => {
+    const bundledCode = await bundler(input);
+    setCode(bundledCode);
+  };
+
+  return (
+    <>
+      <div>
+        <CodeEditor
+          initialValue='const hey = "hello";'
+          onChange={(value) => {
+            if (value) setInput(value);
+          }}
+        />
+        <div>
+          <button onClick={onClick}>Submit</button>
+        </div>
+        <Preview code={code} />
+      </div>
+    </>
+  );
+};
+
+export default CodeCell;
