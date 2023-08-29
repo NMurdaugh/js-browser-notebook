a38fdf253a5f99dcf5d02d664c1adc657d3ff38f
Nick <nmurdaugh42@gmail.com>
2023-08-28 21:46:10 -0500

    extracted bundler code; reorganized project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,15 @@
 import { useState } from 'react';
+import bundler from './bundler';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
-import {
-  inputCodePathResolver,
-  outputCodeFetcher,
-  startEsbuildService,
-} from './plugins';
 
 const App = () => {
   const [input, setInput] = useState('');
   const [code, setCode] = useState('');
 
   const onClick = async () => {
-    const esBuildRef = await startEsbuildService();
-
-    const result = await esBuildRef.build({
-      entryPoints: ['index.js'],
-      sourcemap: 'external',
-      bundle: true,
-      write: false,
-      plugins: [inputCodePathResolver(), outputCodeFetcher(input)],
-      outdir: 'out',
-      define: {
-        // 'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    });
-
-    setCode(result.outputFiles[1].text);
+    const bundledCode = await bundler(input);
+    setCode(bundledCode);
   };
 
   return (
