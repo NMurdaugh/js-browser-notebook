8c9b8471ff6ef730559ec2f39689aec73c423c9d
Nick <nmurdaugh42@gmail.com>
2023-09-02 21:48:06 -0500

    rendered text editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 const App = () => {
   return (
     <>
+      <TextEditor />
       <CodeCell />
     </>
   );
