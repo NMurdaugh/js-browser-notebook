61821328bfa719353b3ecafe1ce1e2592a82e444
Nick <nmurdaugh42@gmail.com>
2023-09-06 17:57:30 -0500

    moveCell action

--- src/features/notebook/notebookSlice.ts
+++ src/features/notebook/notebookSlice.ts
@@ -27,7 +27,23 @@ export const notebookSlice = createSlice({
   name: 'notebook',
   initialState,
   reducers: {
-    moveCell: (state, action: PayloadAction<MoveCellPayload>) => {},
+    moveCell: (state, action: PayloadAction<MoveCellPayload>) => {
+      const { direction } = action.payload;
+
+      const startingIndex = state.order.findIndex(
+        (cellId) => cellId === action.payload.id
+      );
+
+      const targetIndex =
+        direction === 'up' ? startingIndex - 1 : startingIndex + 1;
+
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return;
+      }
+
+      state.order[startingIndex] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+    },
 
     deleteCell: (state, action: PayloadAction<DeleteCellPayload>) => {
       delete state.data.id;
