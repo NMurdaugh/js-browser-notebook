ccb0e13c731c28ca50c707dd2b591b249398284d
Nick <nmurdaugh42@gmail.com>
2023-09-19 17:21:59 -0500

    fixed redux bundler

--- src/features/CodeCell/component.tsx
+++ src/features/CodeCell/component.tsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { useAppDispatch } from '../../app/hooks';
+import React, { useEffect } from 'react';
+import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { Cell, updateCell } from '../Notebook';
 import CodeEditor from './CodeEditor';
 import CodePreview from './CodePreview';
 import ResizableWrapper from './ResizableWrapper';
-// import { createBundle } from './codeBundlesSlice';
-import { bundler } from './services/bundler';
+import { createBundle } from './codeBundlesSlice';
 import './style.css';
 
 interface ICodeCellProps {
@@ -13,18 +12,12 @@ interface ICodeCellProps {
 }
 
 export const CodeCell: React.FC<ICodeCellProps> = ({ cell }) => {
-  const [code, setCode] = useState('');
-  const [error, setError] = useState('');
   const dispatch = useAppDispatch();
-  // const codeBundle = useAppSelector((state) => state.cellBundles[cell.id]);
+  const codeBundle = useAppSelector((state) => state.cellBundles[cell.id]);
 
   useEffect(() => {
     const timer = setTimeout(async () => {
-      // createBundle({ cellId: cell.id, inputCode: cell.content });
-
-      const output = await bundler(cell.content);
-      setCode(output.code);
-      setError(output.error);
+      dispatch(createBundle({ cellId: cell.id, inputCode: cell.content }));
     }, 1000);
 
     return () => {
@@ -47,8 +40,8 @@ export const CodeCell: React.FC<ICodeCellProps> = ({ cell }) => {
         </ResizableWrapper>
 
         <CodePreview
-          code={code}
-          error={error}
+          code={codeBundle?.code}
+          error={codeBundle?.error}
         />
       </div>
     </ResizableWrapper>
