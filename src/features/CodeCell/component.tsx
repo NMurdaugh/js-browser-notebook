3883cb3923fbc2dc93fb6c1ba185f79c1ae19e8c
Nick <116959833+NMurdaugh@users.noreply.github.com>
2023-10-02 18:49:24 +0000

    extracted cumulative code hook

--- src/features/CodeCell/component.tsx
+++ src/features/CodeCell/component.tsx
@@ -1,5 +1,9 @@
 import React, { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../app/hooks';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useCumulativeCode,
+} from '../../app/hooks';
 import { Cell, updateCell } from '../Notebook';
 import CodeEditor from './CodeEditor';
 import CodePreview from './CodePreview';
@@ -14,46 +18,7 @@ interface ICodeCellProps {
 export const CodeCell: React.FC<ICodeCellProps> = ({ cell }) => {
   const dispatch = useAppDispatch();
   const codeBundle = useAppSelector((state) => state.cellBundles[cell.id]);
-  const cumulativeCode = useAppSelector((state) => {
-    const { data, order } = state.notebook;
-    const orderedCells = order.map((id) => data[id]);
-
-    const showFunction = `
-    import _React from 'react';
-    import _ReactDOM from 'react-dom';
-
-    var show = (value) => {
-      const root = document.querySelector('#root')
-
-      if (typeof value === 'object') {
-        if (value.$$typeof && value.props) {
-          _ReactDOM.render(value, root);
-        } else {
-          root.innerHTML = JSON.stringify(value)
-        }
-      } else {
-        root.innerHTML = value
-      }
-    }
-  `;
-
-    const showFunctionNoOp = 'var show = () => {}';
-    const cumulativeCode = [];
-    for (let currentCell of orderedCells) {
-      if (currentCell.type === 'code') {
-        if (currentCell.id === cell.id) {
-          cumulativeCode.push(showFunction);
-        } else {
-          cumulativeCode.push(showFunctionNoOp);
-        }
-        cumulativeCode.push(currentCell.content);
-      }
-      if (currentCell.id === cell.id) {
-        break;
-      }
-    }
-    return cumulativeCode.join('\n');
-  });
+  const cumulativeCode = useCumulativeCode(cell.id);
 
   useEffect(() => {
     if (!codeBundle) {
